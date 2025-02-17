3347c36f2ae8b9e634d4d82f62231dc4d8d88998
yemm <yemm@tuwei.com>
2025-02-17 17:23:53 +0800

    增加ts校验

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import vue from '@vitejs/plugin-vue';
 import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    checker({
+      overlay: false,
+      vueTsc: {
+        tsconfigPath: './tsconfig.app.json',
+      },
+    }),
+  ],
   server: {
     host: '0.0.0.0',
   },
